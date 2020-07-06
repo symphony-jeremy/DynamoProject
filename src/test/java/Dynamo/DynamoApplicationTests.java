b3f3a1e41311f42f1b1c6b5b01e02bbfa428f76a
symphony-boutheina <67713997+symphony-boutheina@users.noreply.github.com>
2020-07-06 14:01:18 +0200

    Spring initializr

---
+++ src/test/java/Dynamo/DynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package Dynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
