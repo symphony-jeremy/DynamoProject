3467605c9929f68d0b46333cdc4ab4950bb95737
symphony-boutheina <67713997+symphony-boutheina@users.noreply.github.com>
2020-07-09 16:42:23 +0200

    creation of tables #2

---
+++ src/main/java/Dynamo/createTable.java
@@ -0,0 +1,55 @@
+package Dynamo;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.TableCollection;
+import com.amazonaws.services.dynamodbv2.model.*;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class createTable {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Videos";
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
+                            // key
+                            new KeySchemaElement("title", KeyType.RANGE)), // Sort key
+                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                            new AttributeDefinition("title", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus() + table.getTableName() );
+
+            TableCollection<ListTablesResult> tables = dynamoDB.listTables();
+            Iterator<Table> iterator = tables.iterator();
+            while (iterator.hasNext()) {
+                Table tble = iterator.next();
+                System.out.println("hi" +tble.getTableName());
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
