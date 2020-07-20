ea497cdd6a12ae69e1736f6499ad17aefbcb637b
symphony-boutheina <67713997+symphony-boutheina@users.noreply.github.com>
2020-07-20 10:44:41 +0200

    reorgornise code #3 #4

--- src/main/java/Dynamo/Tables.java
+++ src/main/java/Dynamo/Tables.java
@@ -1,20 +1,13 @@
 package Dynamo;
-import java.io.IOException;
 import java.util.*;
 
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.TableCollection;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.model.*;
 
-import javax.enterprise.inject.Any;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -27,15 +20,14 @@ public class Tables {
 
     static DynamoDB dynamoDB = new DynamoDB(client);
     static String Name = "Films";
+    GetItemResult result;
 
 
-    public static void main(String[] args) throws Exception {
 
 
-         getItems(Name);   }
 
-
-    static void getTableInformation(String Name ) {
+     // get table information
+    void getTableInformation(String Name ) {
 
         System.out.println("Describing " + Name);
 
@@ -48,7 +40,8 @@ public class Tables {
                 tableDescription.getProvisionedThroughput().getWriteCapacityUnits());
         System.out.println("hey "+   tableDescription.getItemCount());
     }
-    public static void createtable(String Name){
+    // creation of tables
+    public  void createtable(String Name , String key){
 
         /* Create an Object of CreateTableRequest */
         CreateTableRequest request = new CreateTableRequest();
@@ -59,14 +52,14 @@ public class Tables {
         /* Create & Set a list of AttributeDefinition */
         List<AttributeDefinition> attributeDefinitions = Arrays.asList(
                 new AttributeDefinition("ID", ScalarAttributeType.N),
+                new AttributeDefinition(key, ScalarAttributeType.S));
 
-                new AttributeDefinition("Titres", ScalarAttributeType.S));
         request.setAttributeDefinitions(attributeDefinitions);
 
         /* Create & Set a list of KeySchemaElement */
         List<KeySchemaElement> keySchema = Arrays.asList(
                 new KeySchemaElement("ID", KeyType.HASH),
-        new KeySchemaElement("Titres", KeyType.RANGE));
+        new KeySchemaElement(key, KeyType.RANGE));
 
 
         request.setKeySchema(keySchema);
@@ -101,22 +94,8 @@ public class Tables {
 
 
 
-        static void putItem( String Name){
-            Table table = dynamoDB.getTable(Name);
-
-            try {
-                Item item = new Item()
-                        .withPrimaryKey("ID", 1)
-                        .withString("Titres", "La vie est belle");
-                table.putItem(item);
-
-            } catch (Exception e) {
-                System.err.println("Cannot create items.");
-                System.err.println(e.getMessage());
-            }
-
-              }
-            static void getTables(){
+              // get all tables
+             void getTables(){
                 try {
 
                     /* Creating ListTableRequest with limit 50 */
@@ -158,8 +137,8 @@ public class Tables {
 
             }
 
-
-            static void deleteTable(String Name){
+            // delete the table from the database
+             void deleteTable(String Name){
                 DeleteTableRequest request = new DeleteTableRequest();
 
                 /* Setting Table Name */
@@ -186,8 +165,8 @@ public class Tables {
 
                 }
             }
-
-            static void getItems( String Name){
+            // retrieve items from the table
+             GetItemResult getItems(String Name , int ID ,String name){
                 GetItemRequest request = new GetItemRequest();
 
                 /* Setting Table Name */
@@ -197,7 +176,7 @@ public class Tables {
                 request.setReturnConsumedCapacity(ReturnConsumedCapacity.TOTAL);
 
                 /* Setting Name of attributes to Get */
-                request.setProjectionExpression("ID, Titres");
+                request.setProjectionExpression("ID ,  Ville , Distance ");
 
                 /* Setting Consistency Models */
                 /* true for Strong Consistent & false for Eventually Consistent */
@@ -205,13 +184,16 @@ public class Tables {
 
                 /* Create a Map of Primary Key attributes */
                 Map<String, AttributeValue> keysMap = new HashMap<>();
-                keysMap.put("ID", new AttributeValue().withN(String.valueOf(1)));
-                keysMap.put("Titres", (new AttributeValue("La vie est belle")));
+                keysMap.put("ID", new AttributeValue().withN(String.valueOf(ID)));
+                 keysMap.put("Name", new AttributeValue(name));
+
+
+
                 request.setKey(keysMap);
 
                 try {
                     /* Send Get Item Request */
-                    GetItemResult result = client.getItem(request);
+                    result = client.getItem(request);
 
                     System.out.println("Status : " +  result.getSdkHttpMetadata().getHttpStatusCode());
 
@@ -222,14 +204,41 @@ public class Tables {
                                 .forEach(e -> System.out.println(e.getKey() + " " + e.getValue().toString()));
                     }
 
+
                 } catch (AmazonServiceException e) {
 
                     System.out.println("Cannot retrieve"+e.getErrorMessage());
 
                 }
+
+                 return result;
+             }
+              ItemCollection<ScanOutcome> filterTableWithDistance(String Name){
+                Table table = dynamoDB.getTable(Name);
+                Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+                expressionAttributeValues.put(":distance", 500);
+
+                ItemCollection<ScanOutcome> items = table.scan (
+                        "Distance < :distance",                                  //FilterExpression
+                        "ID, Ville , Distance",     //ProjectionExpression
+                        null,                                           //No ExpressionAttributeNames
+                        expressionAttributeValues);
+
+                System.out.println("Scanned " + Name + " to find items over 500KM");
+                Iterator<Item> iterator = items.iterator();
+
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next().toJSONPretty());
+                }
+                return items;
             }
 
             }
+
+
+
+
+
 
 
 
