36de48811c59830cabc93317e72a10403072271c
Jeremy Passeron <jeremy.passeron@symphony.com>
2020-07-24 09:43:10 +0200

    dynamoDB starts automatically during the build

--- src/test/java/Dynamo/MoviesDaoTest.java
+++ src/test/java/Dynamo/MoviesDaoTest.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,6 +18,7 @@ import static org.junit.Assert.*;
 
 
 public class MoviesDaoTest extends DynamoApplicationTests {
+
     static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
             .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
             .build();
@@ -27,62 +29,23 @@ public class MoviesDaoTest extends DynamoApplicationTests {
     private MoviesDao moviesDao;
 
 
-    @Before
+    @BeforeEach
     public void setUp() {
+        // recreate the table before each tests
+        moviesDao.deleteTable("Movies");
         moviesDao.createtable("Movies");
-
-
-    }
-
-    @Test
-    void createTableTest() {
-        TableDescription tableDescription = dynamoDB.getTable("Movies").describe();
-        assertEquals("ACTIVE", tableDescription.getTableStatus());
-
-
-    }
-
-    @Test
-    void putItemTest() {
-        TableDescription tableDescription = dynamoDB.getTable("Movies").describe();
-        assertEquals("ACTIVE", tableDescription.getTableStatus());
+        // insert 2 movies
         Movies movie = new Movies("1", "Forte", "Comédie", "2020", "France");
         moviesDao.putItem("Movies", movie);
         Movies movie1 = new Movies("2", "Limitless", "Action", "2011", "US");
         moviesDao.putItem("Movies", movie1);
-        assertEquals(2, tableDescription.getItemCount().intValue());
-
     }
 
     @Test
-    void filterItemTest() {
-        List<Movies> items = moviesDao.filterTableWithCategory("Movies", "Action");
-        System.out.println("hey" + items);
-        assertEquals(1, items.size());
-
+    void getAllItemsTest() {
+        List<Movies> movies = moviesDao.getAllItems("Movies");
+        assertNotNull(movies);
+        assertEquals(2, movies.size());
     }
-
-    @Test
-    void filterItemwithIdTest() {
-        List<Movies> items = moviesDao.filterTableWithID("Movies", "2");
-        assertEquals("Action", items.get(0).getCategory());
-
-    }
-
-    @Test
-    void deleteItemTest() {
-        moviesDao.deleteItem("Movies", "2");
-        TableDescription tableDescription = dynamoDB.getTable("Movies").describe();
-
-        assertEquals(1, tableDescription.getItemCount().intValue());
-
-
-    }
-
-    @Test
-    void getallitemsTest() {
-        moviesDao.getAllItems("Movies");
-    }
-
 
 }
